90c1b15fe6fd0eea43c80fba0f89c84f51aed2f9
Emma <MarmadileManteater@proton.me>
2023-01-07 12:52:05 -0500

    Move shared code between my sites to a single repo

---
+++ src/models/blog.ts
@@ -0,0 +1,13 @@
+
+export interface IBlogPost {
+  type: 'IBlogPost',
+  id: string,
+  html: string
+  title: string
+  shortDescription: string
+  tags: string[]
+  atime: number
+  mtime: number
+  ctime: number,
+  gittime: number
+}
